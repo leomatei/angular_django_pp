318b48c9cc7fb93d91b3cf63997f2d62d1e59923
unknown <leonard.matei2011@yahoo.com>
2024-07-25 15:22:59 +0300

    don't attach the token for signup

--- frontend/src/app/auth.interceptor.ts
+++ frontend/src/app/auth.interceptor.ts
@@ -10,20 +10,32 @@ import { AuthService } from './services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private excludedUrls: string[] = ['/api/signup'];
+
   constructor(private authService: AuthService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const accessToken = this.authService.getAccessToken();
-    if (accessToken) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
-      });
-      return next.handle(cloned);
-    } else {
+    const shouldExcludeToken = this.excludedUrls.some((url) =>
+      req.url.includes(url)
+    );
+
+    if (shouldExcludeToken) {
+      // If the URL is in the excludedUrls array, pass the request without the JWT
       return next.handle(req);
+    } else {
+      // Otherwise, attach the JWT to the request
+      const accessToken = this.authService.getAccessToken();
+      if (accessToken) {
+        const cloned = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
+        });
+        return next.handle(cloned);
+      } else {
+        return next.handle(req);
+      }
     }
   }
 }
